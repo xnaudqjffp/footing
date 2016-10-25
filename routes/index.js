bff8f4ac4c91e8cd7d80a315eb8a25368ee749d6
DESKTOP-UISO2VL\sanghyun <checkmate617@naver.com>
2016-10-26 02:27:06 +0900

    safasdfasdfdf

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {          //index.hbs 識情
+    title: 'footing',
+    footing: 'Firs'
+  });
 });
+
+router.get('/test', function(req, res, next) {
+  res.render('test', {          //index.hbs 識情
+    data: 'footing'  });
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/free', function(req, res, next) {
+  res.send('message changed');
+});
+
 module.exports = router;
